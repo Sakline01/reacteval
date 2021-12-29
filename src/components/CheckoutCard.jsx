70270bb214dd09f28c2cf02e71fb66b5a605b3ed
Sakline Sk <Sakline_pw1_056@masai.school>
2021-12-29 21:26:06 +0530

    created card of product

---
+++ src/components/CheckoutCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Checkoutcard = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Checkoutcard;
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './productcard.module.css';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const Productcard = () => {
+    return (
+        <div className={styles.card}>
+            <div>
+                <img className={styles.circle} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+            </div>
+            <div className={styles.description}>
+                <div>
+                    <h1>$365</h1>
+                </div>
+                <div className={styles.qty}>
+                    <AddIcon style={{ fontSize: 50 }} />
+                    <h2>2</h2>
+                    <AddIcon style={{ fontSize: 50 }} />
+                </div>
+                <div>
+                    <Button variant="outlined" color="primary" >
+                        Primary
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Productcard;
