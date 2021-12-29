4f165f6a86a7efde943187b3c7f8e99d78182869
Sakline Sk <Sakline_pw1_056@masai.school>
2021-12-29 22:05:32 +0530

    added redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,16 +6,17 @@ const Home = () => {
     useEffect(() => {
         axios.get("http://localhost:3000/products")
             .then(data => {
+                console.log(data.data)
                 setProduct(data.data);
             })
 
 
     }, []);
     return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', flexWrap: 'wrap', rowGap: '15px' }}>
             {
                 product.map(item => (
-                    <Productcard key={item.id} name={item.name} price={item.price} />
+                    <Productcard key={item.id} image={item.image} name={item.name} price={item.price} />
                 ))
             }
         </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import styles from './productcard.module.css';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../redux/action';
 
-const Productcard = ({ name, price }) => {
+const Productcard = ({ price, image }) => {
+    const count = useSelector((state) => state.Count)
+    const dispatch = useDispatch();
     return (
         <div className={styles.card}>
             <div>
-                <img className={styles.circle} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+                <img className={styles.circle} src={image} alt="" />
             </div>
             <div className={styles.description}>
                 <div>
                     <h1>${price}</h1>
                 </div>
                 <div className={styles.qty}>
-                    <AddIcon style={{ fontSize: 50 }} />
-                    <h2>2</h2>
-                    <AddIcon style={{ fontSize: 50 }} />
+                    <AddIcon onClick={() => dispatch(decrement())} style={{ fontSize: 50 }} />
+                    <h2>{count}</h2>
+                    <AddIcon onClick={() => dispatch(increment())} style={{ fontSize: 50 }} />
                 </div>
                 <div>
                     <Button variant="outlined" color="primary" >
